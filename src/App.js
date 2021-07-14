cc65ba136edb8e3790073f7316175f6c54c509db
Praz <t.prazwal@gmail.com>
2021-07-14 22:36:43 +0200

    Add style to the Navbar component Render Navbar in the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+/* eslint-disable import/extensions */
 import React from 'react';
-import Products from './components/Products/Products';
+// import Products from './components/Products/Products';
+// import Navbar from './components/Navbar/Navbar';
+
+import { Navbar, Products } from './components';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,34 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import {
-  AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography
+  AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography,
 } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/commerce.png';
+import useStyles from './styles';
 
-const Navbar = () => (
-  <>
-    <AppBar position="fixed" className={classes.appBar} color="inherit">
-      <Toolbar>
-        <Typography variant="h6" className={classes.title} color="inherit">
-          <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
-          Commerce.js
-        </Typography>
-        <div className={classes.grow} />
-        <div className={classes.button} >
-          <IconButton aria-label="Show cart items" color="inherit">
-            <Badge badgeContent={2} color="secondary">
-              <ShoppingCart />
-            </Badge>
-          </IconButton>
-        </div>
-      </Toolbar>
-    </AppBar>
-  </>
-);
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
+            Commerce.js
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default Navbar;

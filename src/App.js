c0e45646d5ba6e3b1be1ead01256ab3dfd231ce0
Praz <t.prazwal@gmail.com>
2021-07-15 21:28:09 +0200

    fetch the cart from CommerceJS

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { Navbar, Products } from './components';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
+
+  console.log('cart', cart);
 
   return (
     <div>

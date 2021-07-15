6c622b8f5a78939a77609eae76efbbdf6daa397b
Praz <t.prazwal@gmail.com>
2021-07-15 23:40:38 +0200

    add Cart/Cart.jsx to display the contents of the cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+
+const Cart = () => {
+  const isEmpty = true;
+
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">Tou have no items in your shopping cart, start adding some!</Typography>
+  );
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+
+      </Grid>
+    </>
+  );
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+      {isEmpty ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;

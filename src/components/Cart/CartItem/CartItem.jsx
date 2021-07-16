2c4e1435e117b6f3c3b21e0f17cc2467424d3f98
Praz <t.prazwal@gmail.com>
2021-07-16 23:13:44 +0200

    add style to the CartItem component

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -4,26 +4,32 @@ import {
   Typography, Button, Card, CardActions, CardContent, CardMedia,
 } from '@material-ui/core';
 
-const CartItem = ({ item }) => (
-  <Card>
-    <CardMedia image={item.media.source} alt={item.name} />
-    <CardContent>
-      <Typography variant="h4">
-        {item.name}
-      </Typography>
-      <Typography variant="h5">
-        {item.line_total.formatted_with_symbol}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <div>
-        <Button type="button" size="small">-</Button>
-        <Typography>{item.quantity}</Typography>
-        <Button type="button" size="small">+</Button>
-      </div>
-      <Button vvariant="contained" type="button" color="secondary">Remove</Button>
-    </CardActions>
-  </Card>
-);
+import useStyles from './styles';
+
+const CartItem = ({ item }) => {
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">
+          {item.name}
+        </Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small">-</Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">+</Button>
+        </div>
+        <Button variant="contained" type="button" color="secondary">Remove</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default CartItem;

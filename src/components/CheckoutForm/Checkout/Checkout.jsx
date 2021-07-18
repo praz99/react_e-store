6285a429c7bc76d988b04b34d13099f633be51b8
Praz <t.prazwal@gmail.com>
2021-07-18 23:24:00 +0200

    Add style to the Checkout component

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,13 +1,14 @@
-/* eslint-disable no-undef */
 import React, { useState } from 'react';
 import {
   Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button,
 } from '@material-ui/core';
+import useStyles from './styles';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyles();
 
   return (
     <>

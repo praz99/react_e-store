fe6ecc861ec4e4e200772dd29778473ba95b2bd1
Praz <t.prazwal@gmail.com>
2021-07-19 23:19:58 +0200

    Add FormInput inside CheckoutForm

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import {
+  InputLabel, Select, MenuItem, Button, Grid, Typography,
+} from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
 
-const AddressForm = () => (
-  <div>
-    Address Form Here
-  </div>
-);
+const AddressForm = () => {
+  const methods = useForm();
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <FormProvider {...methods}>
+        <form onSubmit="">
+          <Grid container spacing={3}>Hello</Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
 
 export default AddressForm;

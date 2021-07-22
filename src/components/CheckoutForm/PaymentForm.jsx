f660472eb81f2c8a947ef2d06f6a9a1c4ed39c02
Praz <t.prazwal@gmail.com>
2021-07-22 23:59:30 +0200

    add Review component

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CartElement, ElementConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review';
 
-const PaymentForm = () => (
-  <div>
-    Payment Form here
-  </div>
+const PaymentForm = ({ checkoutToken }) => (
+  <>
+    <Review checkoutToken={checkoutToken} />
+  </>
 );
 
 export default PaymentForm;
---
+++ src/components/CheckoutForm/Review.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {
+  Typography, List, ListItem, ListItemText,
+} from '@material-ui/core';
+
+const Review = ({ checkoutToken }) => (
+  <>
+    <Typography variant="h6" gutterBottom>Order summary</Typography>
+    <List disablePadding>
+      {checkoutToken.live.line_items.map((product) => (
+        <ListItem style={{ padding: '10px 0' }} key={product.name}>
+          <ListItemText primary={product.name} secondary={`Quatity: ${product.quantity}`} />
+          <Typography variant="body2">{product.line_total.formatted_with_symbol}</Typography>
+        </ListItem>
+      ))}
+      <ListItem style={{ padding: '10px 0' }}>
+        <ListItemText primary="Total" />
+        <Typograhy variant="subtitle1" style={{ fontWeight: 700 }}>
+
+        </Typograhy>
+      </ListItem>
+    </List>
+  </>
+);
+
+export default Review;

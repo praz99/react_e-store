f3481a06ca52496675607c7e66b4846ded63c872
Praz <t.prazwal@gmail.com>
2021-07-22 17:43:27 +0200

    fetch shipping subdivisions and render in address form

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -23,16 +23,31 @@ const AddressForm = ({ checkoutToken }) => {
       label: name,
     }));
 
-  console.log(countries);
+  const subdivisions = Object.entries(shippingSubdivisions).map(([code, name]) => (
+    {
+      id: code,
+      label: name,
+    }));
+
   const fetchShippingCountries = async (checkoutTokenId) => {
     const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
     setShippingCountries(countries);
     setShippingCountry(Object.keys(countries)[0]);
   };
 
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivision(Object.keys(subdivisions)[0]);
+  };
+
   useEffect(() => {
     fetchShippingCountries(checkoutToken.id);
   }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+  }, [shippingCountry]);
 
   return (
     <>
@@ -61,12 +76,18 @@ const AddressForm = ({ checkoutToken }) => {
                 ))}
               </Select>
             </Grid>
-            {/* <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Subdivision</InputLabel>
-              <Select value="" fullWidth onChange="">
-                <MenuItem key="">
-                  Select Me
-                </MenuItem>
+              <Select
+                value={shippingSubdivision}
+                fullWidth
+                onChange={(e) => setShippingSubdivision(e.target.value)}
+              >
+                {subdivisions.map((subdivision) => (
+                  <MenuItem key={subdivision.id} value={subdivision.id}>
+                    {subdivision.label}
+                  </MenuItem>
+                ))}
               </Select>
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -76,7 +97,7 @@ const AddressForm = ({ checkoutToken }) => {
                   Select Me
                 </MenuItem>
               </Select>
-            </Grid> */}
+            </Grid>
           </Grid>
         </form>
       </FormProvider>
